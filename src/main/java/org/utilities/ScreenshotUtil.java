c3d113d3190e48ae257abdd3979c989949e03772
Jadhavsameer77 <jadhavsameer504@gmail.com>
2023-01-20 20:02:45 +0530

    Change name of screenshot

--- src/main/java/org/utilities/ScreenshotUtil.java
+++ src/main/java/org/utilities/ScreenshotUtil.java
@@ -15,7 +15,7 @@ public class ScreenshotUtil {
     public void takeSnap(WebDriver driver, WebElement element) throws Exception {
         // Take a screenshot of the current web page
         ((JavascriptExecutor)driver).executeScript("arguments[0].style.border='3px solid red'", element);
-        Allure.addAttachment("Any text", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        Allure.addAttachment("Attachment", new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
         File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 
         //Getting Date and Time Format
